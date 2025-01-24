764f31a1ac7039ea7a6f20366936757d9df45f47
ch-shin <cshin23@wisc.edu>
2025-01-24 00:29:51 -0600

    Remove old files

--- scripts/llm_probing_overlap_detection/overlap_probing_experiment.sh
+++
@@ -1,5 +0,0 @@
-python overlap_probing_experiment.py --seed 0 &
-python overlap_probing_experiment.py --seed 1 &
-python overlap_probing_experiment.py --seed 2 &
-python overlap_probing_experiment.py --seed 3 &
-python overlap_probing_experiment.py --seed 4
--- scripts/llm_probing_overlap_detection/overlap_probing_experiment2.sh
+++
@@ -1,6 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0
-python overlap_probing_experiment.py --seed 5 &
-python overlap_probing_experiment.py --seed 6 &
-python overlap_probing_experiment.py --seed 7 &
-python overlap_probing_experiment.py --seed 8 &
-python overlap_probing_experiment.py --seed 9
